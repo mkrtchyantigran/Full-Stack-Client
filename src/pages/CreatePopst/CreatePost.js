4b2175c6c8cfb25b00ddefa3016bd581bfa351a2
Tigran <110290863+mkrtchyantigran@users.noreply.github.com>
2024-12-08 14:36:25 +0100

    First commit

---
+++ src/pages/CreatePopst/CreatePost.js
@@ -0,0 +1,75 @@
+import React, { useContext } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { useEffect} from "react";
+
+import { AuthContext } from "../../helpers/AuthContext";
+
+import "./styles/CreatePost.css"
+
+  
+
+function CreatePost() {
+
+  const { authState } = useContext(AuthContext);
+
+  let navigate = useNavigate();
+  const initialValues = {
+    title: "",
+    postText: "",
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      navigate("/login");
+    }
+  }, []);
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a Title!"),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="title"
+            placeholder="(Ex. Title...)"
+          />
+          <label>Post: </label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(Ex. Post...)"
+          />
+
+          <button type="submit"> Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default CreatePost;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GetPosts from './HomeFunctions'
+
+function Home() {
+  return (
+    <div>
+      <GetPosts />
+    </div>
+  )
+}
+
+export default Home
